b35b7a0cfb59f2e73752fc40cf4c8a874512c0a9
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-28 14:05:11 -0300

    feat: instancia objetos curso1, curso2 e mentoria em main de Main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descrição curso js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+    }
 }
